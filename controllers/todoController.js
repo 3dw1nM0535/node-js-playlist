4cdbdd08d8df7362dba9e1534cd5b8f18fcd26ec
Edwin Moses <workockmoses@gmail.com>
2017-07-06 00:06:41 +0300

    mongo integration

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,15 +1,31 @@
+var mongoose = require('mongoose');
+
+var bodyParser = require('body-parser');
+
+var data = [
+  {item: 'get milk'},
+  {item: 'walk dog'},
+  {item: 'kick some coding ass'}
+];
+
+var urlencodedbodyParser = bodyParser.urlencoded({extended: false });
+
 module.exports = (app) => {
 
   app.get('/todo', (req, res) => {
-    res.render('todo');
+    res.render('todo', {todos: data });
   });
 
-  app.post('/todo', (req, res) => {
-
+  app.post('/todo', urlencodedbodyParser, (req, res) => {
+    data.push(req.body);
+    res.json(data);
   });
 
-  app.delete('/todo', (res, req) => {
-
+  app.delete('/todo/:item', (res, req) => {
+    data = data.filter((todo) => {
+      return todo.item.replace(/ /g, '-') !== req.params.item;
+    });
+    res.json(data);
   });
 
 };
