13cef8cc57fe61daca32f8b02ba491cf7352d846
Edwin Moses <workockmoses@gmail.com>
2017-07-05 17:33:35 +0300

    app controller

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var todoController = require('./controllers/todoController');
+
+var app = express();
+
+var port = 3000;
+
+//set templating engine
+app.set('view eingine', 'ejs');
+
+//static files handler
+app.use(express.static('/public'));
+
+//todoController model
+todoController(app);
+
+//listening port
+app.listen(port);
+console.log(`You are listening on port ${port}`);
