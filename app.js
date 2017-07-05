9b971a53d26c7d342da24ec51ab3ac3eca8a7a93
Edwin Moses <workockmoses@gmail.com>
2017-07-05 11:37:45 +0300

    serving html pages

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ var port = 3000;
 var server = http.createServer((req, res) => {
   console.log(`Request received from browser ${req.url}`);
   res.writeHead(200, {'Content-Type': 'application/json' });
-
+  var myObj = {
+    name: 'Edwin Moses',
+    age: 22,
+    gender: 'Male'
+  };
+  res.end(JSON.stringify(myObj));
 });
 
 server.listen(port, '127.0.0.1');
