df34d3ec83ff717c55e06065f6a08755a3ecef08
Edwin Moses <workockmoses@gmail.com>
2017-07-05 12:01:30 +0300

    added contact page and error catch code

--- app.js
+++ app.js
@@ -8,7 +8,21 @@ var server = http.createServer((req, res) => {
   if(req.url === '/home' || req.url === '/') {
     res.writeHead(200, {'Content-Type': 'text/html' });
     fs.createReadStream(__dirname + '/index.html').pipe(res);
-  };
+  } else if(req.url === '/contact') {
+    res.writeHead(200, {'Content-Type': 'text/html' });
+    fs.createReadStream(__dirname + '/contact.html').pipe(res);
+  } else if(req.url === '/api/user') {
+    res.writeHead(200, {'Content-Type': 'application/json' });
+    var user = [
+      {name: 'EdwinMoses', age: 22, gender: 'Male'},
+      {name: 'Mikeson', age: 22, gender: 'Male'},
+      {name: 'Grayson', age: 21, gender: 'Male'}
+    ];
+    res.end(JSON.stringify(user));
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/html' });
+    fs.createReadStream(__dirname + '/404.html');
+  }
 });
 
 server.listen(port, '127.0.0.1');
