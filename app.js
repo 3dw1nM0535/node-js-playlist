134e6eba036f1d2c30083c6abcad01c1bc034196
Edwin Moses <workockmoses@gmail.com>
2017-07-05 11:43:30 +0300

    stream html pages

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ var port = 3000;
 
 var server = http.createServer((req, res) => {
   console.log(`Request received from browser ${req.url}`);
-  res.writeHead(200, {'Content-Type': 'application/json' });
-  var myObj = {
-    name: 'Edwin Moses',
-    age: 22,
-    gender: 'Male'
+  if(req.url === '/home' || req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/html' });
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
   };
-  res.end(JSON.stringify(myObj));
 });
 
 server.listen(port, '127.0.0.1');
