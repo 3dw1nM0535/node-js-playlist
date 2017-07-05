a9a15f2ac591368f655ccc49299ff8d2b1b3b40a
Edwin Moses <workockmoses@gmail.com>
2017-07-05 11:05:51 +0300

    send readStream to user

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 var http = require('http');
 var fs = require('fs');
 
-var myStream = fs.createReadStream(__dirname + '/readAble.txt', 'utf8');
-var writeStream = fs.createWriteStream(__dirname + '/witeMe.txt');
-
-myStream.pipe(writeStream);
-/*
 var port = 3000;
 
 var server = http.createServer((req, res) => {
   console.log(`Request received from browser ${req.url}`);
   res.writeHead(200, {'Content-Type': 'text/plain' });
-  res.end('Node server running in background!');
+  var myStream = fs.createReadStream(__dirname + '/readAble.txt', 'utf8');
+  myStream.pipe(res);
+
 });
 
 server.listen(port, '127.0.0.1');
 console.log(`Server listening on port ${port}`);
-*/
