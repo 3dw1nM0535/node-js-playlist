a16cce2f4c6a9f468a85aa341d225a9c871dc77e
Edwin Moses <workockmoses@gmail.com>
2017-07-05 12:48:17 +0300

    express smart

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ var port = 3000;
 
 var app = express();
 
+app.get('/', (req, res) => {
+  res.send('This is the homepage!');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('This is the contact page!');
+});
+
 app.listen(port);
